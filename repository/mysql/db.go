a746b0dcf1b4604bb4b80b51388ca52318eb04dd
nima abdpour <nimaabdpoor@gmail.com>
2023-12-23 00:05:58 +0330

    repository for db created

---
+++ repository/mysql/db.go
@@ -0,0 +1,4 @@
+package mysql
+
+type DB struct {
+}
---
+++ repository/mysql/user.go
@@ -0,0 +1,11 @@
+package mysql
+
+import entity "Game/entity/user"
+
+func (db DB) IsEmailUnique(email string) (bool, error) {
+
+}
+
+func (db DB) Register(user entity.User) (entity.User, error) {
+
+}
