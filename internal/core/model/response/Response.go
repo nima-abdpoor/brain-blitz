32df4207d8fee95216441137ffba5947ab89dff3
nima-abdpour <nimaabdpoor@gmail.com>
2023-12-26 13:17:52 +0330

    SignUp request and response

---
+++ internal/core/model/response/Response.go
@@ -0,0 +1,10 @@
+package response
+
+import "BrainBlitz.com/game/internal/core/entity/error_code"
+
+type Response struct {
+	Data         interface{}          `json:"data"`
+	Status       bool                 `json:"status"`
+	ErrorCode    error_code.ErrorCode `json:"errorCode"`
+	ErrorMessage string               `json:"errorMessage"`
+}
