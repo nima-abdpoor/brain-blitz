95381331bb8da45046289447955fbae762600e99
nima abdpour <nimaabdpoor@gmail.com>
2023-12-26 21:04:43 +0330

    concrete user repository created.

--- internal/infra/repository/user.go
+++ internal/infra/repository/user.go
@@ -3,6 +3,27 @@ package repository
 import (
 	"BrainBlitz.com/game/internal/core/dto"
 	"BrainBlitz.com/game/internal/core/port/repository"
+	"errors"
+	"strings"
+)
+
+const (
+	insertUserStatement = "INSERT INTO User ( " +
+		"`email`, " +
+		"`password`, " +
+		"`display_name`, " +
+		"`created_at`," +
+		"`updated_at`) " +
+		"VALUES (?, ?, ?, ?, ?)"
+)
+
+const (
+	duplicateEntryMsg = "Duplicate entry"
+	numberRowInserted = 1
+)
+
+var (
+	insertUserErr = errors.New("failed to insert user")
 )
 
 type UserRepository struct {
@@ -10,5 +31,27 @@ type UserRepository struct {
 }
 
 func (ur UserRepository) Insert(dto dto.UserDTO) error {
+	result, err := ur.DB.GetDB().Exec(insertUserStatement,
+		dto.Email,
+		dto.Password,
+		dto.DisplayName,
+		dto.CreatedAt,
+		dto.UpdatedAt,
+	)
 
+	if err != nil {
+		if strings.Contains(err.Error(), duplicateEntryMsg) {
+			return repository.DuplicateUser
+		}
+		return err
+	}
+	numRow, err := result.RowsAffected()
+
+	if err != nil {
+		return err
+	}
+	if numRow != numberRowInserted {
+		return insertUserErr
+	}
+	return nil
 }
