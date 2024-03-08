48b1436c4cdaec0945dda14f6fb5742e41b28e84
nima abdpour <nimaabdpoor@gmail.com>
2024-03-08 10:10:25 +0330

    update migration query

--- internal/infra/repository/migration/init.sql
+++ internal/infra/repository/migration/init.sql
@@ -1,9 +1,9 @@
 CREATE TABLE user
 (
-    "id"           bigserial NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    "username"     varchar   NOT NULL,
-    "password"     varchar   NOT NULL,
-    "display_name" varchar   NOT NULL,
-    "created_at"   TIMESTAMP,
-    "updated_at"   TIMESTAMP
+    `id`           bigint primary key AUTO_INCREMENT PRIMARY KEY,
+    `username`     varchar(100) NOT NULL,
+    `password`     varchar(255) NOT NULL,
+    `display_name` varchar(100) NOT NULL,
+    `created_at`   TIMESTAMP,
+    `updated_at`   TIMESTAMP
 );
