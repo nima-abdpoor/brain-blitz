84d0ced63dc6853a024e1f10e1b2aa7c631f3a3c
n.abdpoor <nimaabdpoor@gmail.com>
2024-05-26 16:16:02 +0330

    unit test for Auth middleware completed

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/knadh/koanf/v2 v2.1.1
 	github.com/labstack/echo/v4 v4.12.0
 	github.com/redis/go-redis/v9 v9.5.1
+	github.com/stretchr/testify v1.8.4
 	github.com/thoas/go-funk v0.9.3
 	go.mongodb.org/mongo-driver v1.14.0
 	go.uber.org/zap v1.26.0
@@ -23,6 +24,7 @@ require (
 
 require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.0.0-alpha.1 // indirect
@@ -38,6 +40,7 @@ require (
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
---
+++ internal/middleware/auth_test.go
@@ -0,0 +1,138 @@
+package middleware
+
+import (
+	"BrainBlitz.com/game/internal/core/port/service"
+	auth "BrainBlitz.com/game/internal/core/service"
+	middlewareConsts "BrainBlitz.com/game/internal/middleware/constants"
+	"BrainBlitz.com/game/pkg/errmsg"
+	"errors"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestAuth(t *testing.T) {
+	e := echo.New()
+
+	createRequest := func(method, target string, headers map[string]string) *http.Request {
+		req := httptest.NewRequest(method, target, strings.NewReader(""))
+		for key, value := range headers {
+			req.Header.Set(key, value)
+		}
+		return req
+	}
+
+	tests := []struct {
+		name               string
+		idParam            string
+		token              string
+		validateTokenRes   bool
+		validateTokenData  map[string]interface{}
+		validateTokenError error
+		expectedStatusCode int
+		expectedResponse   string
+		authGenerator      service.AuthGenerator
+		expectUserId       string
+		expectRole         string
+	}{
+		{
+			name:               "Invalid Id",
+			idParam:            "0",
+			expectedStatusCode: http.StatusBadRequest,
+			expectedResponse:   "Invalid Id",
+		},
+		{
+			name:               "Invalid Id",
+			idParam:            "",
+			expectedStatusCode: http.StatusBadRequest,
+			expectedResponse:   "Invalid Id",
+		},
+		{
+			name:               "Missing Authorization Header",
+			idParam:            "123",
+			expectedStatusCode: http.StatusForbidden,
+			expectedResponse:   errmsg.InvalidAuthentication,
+		},
+		{
+			name:               "Invalid Token",
+			idParam:            "123",
+			token:              "invalid-token",
+			validateTokenError: errors.New("invalid token"),
+			expectedStatusCode: http.StatusForbidden,
+			expectedResponse:   errmsg.InvalidAuthentication,
+		},
+		{
+			name:               "Valid Token but Invalid User",
+			idParam:            "123",
+			token:              "valid-token",
+			validateTokenRes:   true,
+			validateTokenData:  map[string]interface{}{"user": "456", "role": "admin"},
+			expectedStatusCode: http.StatusForbidden,
+			expectedResponse:   errmsg.AccessDenied,
+		},
+		{
+			name:               "Valid Token and Valid User",
+			idParam:            "123",
+			token:              "valid-token",
+			validateTokenRes:   true,
+			validateTokenData:  map[string]interface{}{"user": "123", "role": "admin"},
+			expectedStatusCode: http.StatusOK,
+			expectedResponse:   "next",
+			expectUserId:       "123",
+			expectRole:         "admin",
+		},
+		{
+			name:               "Valid Token and Valid User",
+			idParam:            "123",
+			token:              "valid-token",
+			validateTokenRes:   true,
+			validateTokenData:  map[string]interface{}{"user": "123", "role": "admin"},
+			expectedStatusCode: http.StatusOK,
+			expectedResponse:   "next",
+			expectUserId:       "123",
+			expectRole:         "admin",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req := createRequest(http.MethodGet, "/", map[string]string{
+				"Authorization": tt.token,
+			})
+			rec := httptest.NewRecorder()
+			c := e.NewContext(req, rec)
+			c.SetParamNames("id")
+			c.SetParamValues(tt.idParam)
+
+			next := func(c echo.Context) error {
+				return c.String(http.StatusOK, "next")
+			}
+			middleware := Auth(getMockedAuthGenerator(tt.validateTokenRes, tt.validateTokenError, tt.validateTokenData))(next)
+			err := middleware(c)
+			if err != nil {
+				assert.Error(t, err)
+			}
+			assert.Equal(t, tt.expectedStatusCode, rec.Code)
+			assert.Contains(t, rec.Body.String(), tt.expectedResponse)
+			if tt.expectedStatusCode == http.StatusOK {
+				userClaim := c.Get(middlewareConsts.UserId).(auth.Claim)
+				assert.Equal(t, tt.expectUserId, userClaim.UserId)
+				assert.Equal(t, tt.expectRole, userClaim.Role)
+			}
+		})
+	}
+}
+
+func getMockedAuthGenerator(shouldTokenBeValid bool, err error, data map[string]interface{}) service.AuthGenerator {
+	return service.NewMockAuthGenerator(
+		func(d []string, token string) (bool, map[string]interface{}, error) {
+			return shouldTokenBeValid, data, err
+		}, func(data map[string]string) (string, error) {
+			return "", nil
+		}, func(data map[string]string) (string, error) {
+			return "", nil
+		})
+}
