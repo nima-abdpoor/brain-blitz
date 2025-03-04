3289254b81bb6a5e4612f3cf9ce5cd56e85b8202
n.abdpoor <nimaabdpoor@gmail.com>
2025-03-04 13:59:13 +0330

    changes

--- match_app/repository/match.go
+++ match_app/repository/match.go
@@ -1,1 +1,58 @@
 package repository
+
+import (
+	"BrainBlitz.com/game/internal/infra/repository/redis"
+	"BrainBlitz.com/game/match_app/service"
+	"context"
+	"fmt"
+	"log/slog"
+	"strconv"
+	"time"
+)
+
+type Config struct {
+	WaitingListPrefix           string        `koanf:"waitingListPrefix"`
+	MinTimeWaitingListSelection time.Duration `koanf:"mint_time_list_selection"`
+}
+
+type MatchMakingRepository struct {
+	Config Config
+	Logger *slog.Logger
+	db     *redis.Adapter
+}
+
+func NewUserRepository(config Config, logger *slog.Logger) service.Repository {
+	return MatchMakingRepository{
+		Config: config,
+		Logger: logger,
+	}
+}
+
+func (m MatchMakingRepository) AddToWaitingList(ctx context.Context, category service.Category, userId string) error {
+	err := redis.ZAdd(m.db.Client(),
+		fmt.Sprintf("%s:%v", m.Config.WaitingListPrefix, category),
+		float64(time.Now().UnixMicro()),
+		userId,
+	)
+	return err
+}
+
+func (m MatchMakingRepository) GetWaitingListByCategory(ctx context.Context, category service.Category) ([]service.WaitingMember, error) {
+	key := fmt.Sprintf("%s:%v", m.Config.WaitingListPrefix, category)
+	mintTime := strconv.Itoa(int(time.Now().Add(m.Config.MinTimeWaitingListSelection).UnixMicro()))
+	maxTime := strconv.Itoa(int(time.Now().UnixMicro()))
+	if list, err := redis.ZRange(ctx, m.db.Client(), key, mintTime, maxTime); err != nil {
+		return nil, err
+	} else {
+		result := make([]service.WaitingMember, 0)
+		for _, z := range list {
+			userId, _ := strconv.Atoi(z.Member.(string))
+			result = append(result, service.WaitingMember{
+				UserId:    uint(userId),
+				TimeStamp: int64(z.Score),
+				Category:  category,
+			})
+		}
+		return result, nil
+	}
+}
--- match_app/service/service.go
+++ match_app/service/service.go
@@ -1,1 +1,10 @@
 package service
+
+import (
+	"context"
+)
+
+type Repository interface {
+	AddToWaitingList(ctx context.Context, category Category, userId string) error
+	GetWaitingListByCategory(ctx context.Context, category Category) ([]WaitingMember, error)
+}
