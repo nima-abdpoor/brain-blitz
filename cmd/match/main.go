6139e55057dd3b35e7422127e7a97b19f56b5523
n.abdpoor <nimaabdpoor@gmail.com>
2025-03-05 11:52:27 +0330

    main

---
+++ cmd/match/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"BrainBlitz.com/game/match_app"
+	cfgloader "BrainBlitz.com/game/pkg/cfg_loader"
+	"BrainBlitz.com/game/pkg/logger"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	var cfg match_app.Config
+	workingDir, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("Error getting current working directory: %v", err)
+	}
+
+	options := cfgloader.Option{
+		Prefix:       "MATCH_",
+		Delimiter:    ".",
+		Separator:    "__",
+		YamlFilePath: filepath.Join(workingDir, "deploy", "user", "development", "config.yaml"),
+		CallbackEnv:  nil,
+	}
+
+	if err := cfgloader.Load(options, &cfg); err != nil {
+		log.Fatalf("Failed to load matchApp config: %v", err)
+	}
+
+	logger.Init(cfg.Logger)
+	matchLogger := logger.L()
+
+	matchLogger.Info("match_logger service started...")
+
+	app := match_app.Setup(cfg, matchLogger)
+	app.Start()
+}
--- match_app/app.go
+++ match_app/app.go
@@ -6,14 +6,22 @@ import (
 	"BrainBlitz.com/game/match_app/repository"
 	"BrainBlitz.com/game/match_app/service"
 	httpserver "BrainBlitz.com/game/pkg/http_server"
+	"context"
+	"fmt"
 	"log/slog"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 )
 
 type Application struct {
+	Config      Config
 	Repository  service.Repository
 	Service     service.Service
 	UserHandler http.Handler
 	HTTPServer  http.Server
+	Logger      *slog.Logger
 }
 
 func Setup(config Config, logger *slog.Logger) Application {
@@ -23,9 +31,77 @@ func Setup(config Config, logger *slog.Logger) Application {
 	handler := http.NewHandler(srv)
 
 	return Application{
+		Config:      config,
 		Repository:  repo,
 		Service:     srv,
 		UserHandler: handler,
 		HTTPServer:  http.New(httpserver.New(config.HTTPServer), handler, logger),
+		Logger:      logger,
+	}
+}
+
+func (app Application) Start() {
+	var wg sync.WaitGroup
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	startServers(app, &wg)
+	<-ctx.Done()
+	app.Logger.Info("Shutdown signal received...")
+
+	shutdownTimeoutCtx, cancel := context.WithTimeout(context.Background(), app.Config.TotalShutdownTimeout)
+	defer cancel()
+
+	if app.shutdownServers(shutdownTimeoutCtx) {
+		app.Logger.Info("Servers shut down gracefully")
+	} else {
+		app.Logger.Warn("Shutdown timed out, exiting application")
+		os.Exit(1)
+	}
+
+	wg.Wait()
+	app.Logger.Info("user_app stopped")
+}
+
+func startServers(app Application, wg *sync.WaitGroup) {
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		app.Logger.Info(fmt.Sprintf("HTTP server started on %d", app.Config.HTTPServer.Port))
+		if err := app.HTTPServer.Serve(); err != nil {
+			// todo add metrics
+			app.Logger.Error(fmt.Sprintf("error in HTTP server on %d", app.Config.HTTPServer.Port), "error", err)
+		}
+		app.Logger.Info(fmt.Sprintf("HTTP server stopped %d", app.Config.HTTPServer.Port))
+	}()
+}
+
+func (app Application) shutdownServers(ctx context.Context) bool {
+	shutdownDone := make(chan struct{})
+
+	go func() {
+		var shutdownWg sync.WaitGroup
+		shutdownWg.Add(1)
+		go app.shutdownHTTPServer(&shutdownWg)
+
+		shutdownWg.Wait()
+		close(shutdownDone)
+	}()
+
+	select {
+	case <-shutdownDone:
+		return true
+	case <-ctx.Done():
+		return false
+	}
+}
+
+func (app Application) shutdownHTTPServer(wg *sync.WaitGroup) {
+	defer wg.Done()
+	httpShutdownCtx, httpCancel := context.WithTimeout(context.Background(), app.Config.HTTPServer.ShutDownCtxTimeout)
+	defer httpCancel()
+	if err := app.HTTPServer.Stop(httpShutdownCtx); err != nil {
+		app.Logger.Error(fmt.Sprintf("HTTP server graceful shutdown failed: %v", err))
 	}
 }
