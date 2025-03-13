6054ab20e66f93bd3c160ab0eec500dc34d9b5e0
nima-abdpoor <nimaabdpoor@gmail.com>
2025-03-13 16:22:18 +0330

    remove unused code

--- config/config.go
+++ config/config.go
@@ -1,13 +1,11 @@
 package config
 
 import (
-	"BrainBlitz.com/game/adapter/broker/kafka"
 	"BrainBlitz.com/game/feature"
 	"BrainBlitz.com/game/internal/core/server/http"
 	"BrainBlitz.com/game/internal/core/service"
 	presenceService "BrainBlitz.com/game/internal/core/service/presence"
 	"BrainBlitz.com/game/internal/infra/repository"
-	"BrainBlitz.com/game/internal/infra/repository/mongo"
 	"BrainBlitz.com/game/internal/infra/repository/presence"
 	"BrainBlitz.com/game/internal/infra/repository/redis"
 )
@@ -17,9 +15,7 @@ type Config struct {
 	HTTPServer  http.Config            `koanf:"http"`
 	Auth        service.Config         `koanf:"auth"`
 	Mysql       repository.Config      `koanf:"mysql"`
-	Mongo       mongo.Config           `koanf:"mongo"`
 	Redis       redis.Config           `koanf:"redis"`
 	Presence    presenceService.Config `koanf:"presence_service"`
 	GetPresence presence.Config        `koanf:"presence"`
-	Kafka       kafka.Config           `koanf:"kafka"`
 }
